c0771ff0bcf93c3963ca4cb65b6e417e5cdb7bb1
Dushal123 <kelladushal501@gmail.com>
2025-08-28 20:15:36 +0530

    Display-classes.cpp

---
+++ Display-classes.cpp
@@ -0,0 +1,23 @@
+//Dushal kella
+//24070123053
+
+#include <iostream>
+using namespace std;
+
+class Cuboid {
+public:
+    int h, w, l;
+    void input() {
+        cout << "Enter height, width, length: ";
+        cin >> h >> w >> l;
+    }
+    int volume() { return h * w * l; }
+    void disp() { cout << "Volume of cuboid: " << volume(); }
+};
+
+int main() {
+    Cuboid c1;
+    c1.input();
+    c1.disp();
+    return 0;
+}
