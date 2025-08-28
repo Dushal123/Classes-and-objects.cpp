5f08c2565def403a63ca3bfbe5dd1926b4370ae9
Dushal123 <kelladushal501@gmail.com>
2025-08-28 20:07:43 +0530

    Basic-classes-and-object.cpp

---
+++ Basic-classes-and-object.cpp
@@ -0,0 +1,21 @@
+//Dushal kella
+//24070123053
+#include <iostream>
+using namespace std;
+
+class Cuboid {
+    int l, b, h;
+public:
+    Cuboid(int x, int y, int z) {
+        l = x; b = y; h = z;
+    }
+    int volume() {
+        return l * b * h;
+    }
+};
+
+int main() {
+    Cuboid c(5, 7, 7);
+    cout << "Volume = " << c.volume() << endl;
+    return 0;
+}
