fbf20978d923550276d06c83ddb41144d3e4d8ad
Dushal123 <kelladushal501@gmail.com>
2025-08-28 20:13:02 +0530

    Function-classes.cpp

---
+++ Function-classes.cpp
@@ -0,0 +1,17 @@
+//Dushal kella
+//24070123053
+
+#include <iostream>
+using namespace std;
+
+class Cube {
+public:
+    int h = 6, w = 4, l = 5;
+    int volume() { return h * w * l; }
+};
+
+int main() {
+    Cube c;
+    cout << "Volume of the cube is: " << c.volume() << endl;
+    return 0;
+}
